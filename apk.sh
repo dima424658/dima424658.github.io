91ae3e53eda7f2c44ade8bd5c6e4ba88b14f3ef5
Dmitry Pankov <dmitry@enula.ru>
2026-05-18 23:17:28 +0300

    update packages

--- apk.sh
+++ apk.sh
@@ -5,6 +5,8 @@ REPO="${REPO:=${HOME}/Git/openwrt}"
 CONFIG_USE_APK="y" make package/sing-box-extended/compile package/podkop/compile package/luci-app-podkop/compile -j28 -C ${REPO}
 CONFIG_USE_APK="" make package/sing-box-extended/compile package/podkop/compile package/luci-app-podkop/compile -j28 -C ${REPO}
 
+rm *.apk *.ipk
+
 cp ${REPO}/bin/packages/aarch64_cortex-a53/base/{sing-box*,*podkop*} .
 
 ${REPO}/staging_dir/host/bin/apk mkndx --root . --keys-dir . --allow-untrusted --sign ./keys/private-key.pem --output packages.adb *.apk
